9ae1070552111a1e6e6d0258ccc4c663647596aa
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-02-17 09:52:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="disnake-pagination",
-    version="0.0.1",
+    version="0.0.2",
     description="Easily create pagination for your embeds.",
     long_description=long_description,
     long_description_content_type='text/markdown',
